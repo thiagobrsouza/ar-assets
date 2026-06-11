d9c0a55ee9d292a39994d55554e5c05df61c9a46
Thiago Souza <tbs.souza@outlook.com>
2026-06-11 12:11:25 -0300

    Projeto fase 1: Criando estrutura base

---
+++ api/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
